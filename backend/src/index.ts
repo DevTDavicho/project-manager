7791658c468eef856612f16da2eb64868c5cc8bf
DevTDavicho <developer2023Davicho@outlook.es>
2025-08-01 17:59:12 -0500

    Modified backend routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,37 @@
 // src/index.ts
 
+// Importaciones principales
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+// import { GetCommand } from '@aws-sdk/lib-dynamodb';
 
+// Configuraciones
 dotenv.config();
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Rutas de prueba
 app.get('/', (req, res) => {
     res.send('✅ Backend funcionando correctamente');
 });
 
-// Rutas
+// Rutas de la aplicacion
 import authRoutes from './routes/auth';
 app.use('/api/auth', authRoutes);
 
 import userRoutes from './routes/users';
 app.use('/api/users', userRoutes);
 
-// Escuchar el servidor
-const PORT = process.env.PORT || 3000;
+// Conexion a mongodb (temproal para prueba)
+/*import connectDB from './config/db';
+connectDB(); // Asegúrate de llamar a la función*/
+
+// Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
